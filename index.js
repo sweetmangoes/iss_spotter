440b6cac40c5578ef5bd9f04078bf08a44df2191
sweetmangoes <alphonsechristian@gmail.com>
2022-10-24 20:41:56 -0400

    adding index2 and iss_promised

--- index.js
+++ index.js
@@ -50,3 +50,5 @@ nextISSTimesForMyLocation((error, passTimes) => {
   // success, print out the deets!
   printPassTimes(passTimes); 
 });
+
+module.exports = {printPassTimes}; 
---
+++ index2.js
@@ -0,0 +1,15 @@
+const {nextISSTimesForMyLocation} = require('./iss_promised'); 
+const {printPassTimes} = require('./index'); 
+
+// fetchMyIp()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then(body => console.log(body));
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
